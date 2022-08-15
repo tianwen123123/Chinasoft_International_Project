b289f76fba3fa3163b3d16d400962b97dcd008e5
duxin00 <91530385+duxin00@users.noreply.github.com>
2022-08-15 14:29:21 +0800

    dx

--- classify_project/classify_parent/classify_backend/src/main/webapp/classify/pages/GarbageSorting/GarbageSorting.js
+++ classify_project/classify_parent/classify_backend/src/main/webapp/classify/pages/GarbageSorting/GarbageSorting.js
@@ -25,27 +25,50 @@ Page({
           Filepath:res.tempFiles[0].tempFilePath
         })
         console.log(res.tempFiles[0].tempFilePath)
-        wx.uploadFile({
-          filePath: that.data.Filepath,
-          name: 'imagefile',
-          url: '',
-          success: (result) => {
-            wx.navigateTo({
-              url: '../result/result?Filepath='+that.data.Filepath,
-            })
-          },
-          fail: (res) => {},
-          complete: (res) => {
-            wx.navigateTo({
-              url: '../result/result?Filepath='+that.data.Filepath,
-            })
-          },
-        })
       },
       fail: (res) => {},
       complete: (res) => {},
     })
   },
+
+  regition(){
+    var that = this
+    wx.uploadFile({
+      url: '',
+      filePath: that.data.Filepath,
+      name: 'imagefile',
+      success: (res) => {
+        var message = res.message
+        console.log(res)
+        if(res.data.flag==true){
+            wx.showToast({
+              title: message,
+              icon:'none',
+              duration:1000
+            })
+        }else{
+          wx.showToast({
+            title: message,
+            icon:'none',
+            duration:1000
+          })
+        }
+
+        // wx.navigateTo({
+        //   url: '../result/result?Filepath='+that.data.Filepath,
+        // })
+      },
+      fail: (res) => {},
+      complete: (res) => {
+        // wx.navigateTo({
+        //   url: '../result/result?Filepath='+that.data.Filepath,
+        // })
+      },
+    })
+  },
+
+
+
   VedioRecognition(){
     var that = this
     wx.chooseMedia({
