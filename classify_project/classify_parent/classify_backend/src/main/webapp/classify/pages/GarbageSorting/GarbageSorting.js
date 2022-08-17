aafbcf9ee076f439cc9d863bf0b0602a54605a9f
tianwen123123 <2391908465@qq.com>
2022-08-17 17:01:46 +0800

    rby

--- classify_project/classify_parent/classify_backend/src/main/webapp/classify/pages/GarbageSorting/GarbageSorting.js
+++ classify_project/classify_parent/classify_backend/src/main/webapp/classify/pages/GarbageSorting/GarbageSorting.js
@@ -31,32 +31,40 @@ Page({
           isshow:false
         })
         console.log(res.tempFiles[0].tempFilePath)
+        wx.uploadFile({
+          url: getApp().globalData.webUrl+'/picture',
+          filePath: that.data.Filepath,
+          formData: {
+            'telephone':that.data.telephone
+          },
+          method:'post',
+          name: 'imgFile',
+          success:(res)=>{
+            console.log(res)
+            var jsonObj=JSON.parse(res.data)
+            if(jsonObj.flag==true){
+              wx.showToast({
+                title: jsonObj.message,
+                icon:"none",
+                duration:1000
+              })
+            }else{
+              wx.showToast({
+                title: jsonObj.message,
+                icon:'none',
+                duration:1000,
+              })
+            }
+          },
+          fail:(res)=>{
+            console.log("上传失败")
+          }
+        })
       },
       fail: (res) => {},
       complete: (res) => {},
     })
-    wx.uploadFile({
-      url: getApp().globalData.webUrl+'/picture?telephone'+that.data.telephone,
-      filePath: that.data.Filepath,
-      name: 'imgFile',
-      success:(res)=>{
-        console.log(res)
-        let message = res.message
-        if(res.data.flag==true){
-          wx.showToast({
-            title: message,
-            icon:"none",
-            duration:1000
-          })
-        }else{
-          wx.showToast({
-            title: message,
-            icon:'none',
-            duration:1000,
-          })
-        }
-      },
-    })
+
   },
   Realtimerecogn(){
 
@@ -71,19 +79,15 @@ Page({
         })
         return 
     }
-    that.setData({
-      resultimgpath:that.data.Filepath,
-      isshow:true
-    })
     wx.showLoading({
       title: '识别中',
       mask: true,
     })
     wx.request({
-      url: 'url',
-      method:'',
+      url: getApp().globalData.webUrl+'/picture?telephone='+that.data.telephone,
+      method:'get',
       success: (res) => {
-        var message = res.message
+        var message = res.data.message
         console.log(res)
         if(res.data.flag==true){
           wx.hideLoading({
@@ -96,6 +100,10 @@ Page({
             title: message,
             icon:'none',
             duration:1000
+          })
+          that.setData({
+            resultimgpath: "http://rgbvrgbry.hb-bkt.clouddn.com/"+res.data.data,
+            isshow:true
           })
         }else{
           wx.showToast({
