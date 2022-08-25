4a9edab79dfae324f4dd290d37c5b9575d12739f
duxin00 <91530385+duxin00@users.noreply.github.com>
2022-08-25 13:26:20 +0800

    dx

--- recognization_project/recognization_parent/recognization_backend/src/main/webapp/recognization/pages/liveRecognization/liveRecognization.js
+++ recognization_project/recognization_parent/recognization_backend/src/main/webapp/recognization/pages/liveRecognization/liveRecognization.js
@@ -6,17 +6,21 @@ Page({
     isshow: false,
     startopen: true,
     stopopen: false,
+    startopen1:true,
+    stopopen2:false,
     resultinfo: [],
     title: "http://rh5dq0hiv.hb-bkt.clouddn.com/",
     flag: false,
     radioItems: [{
-        name: '使用1',
-        value: '美国',
-        checked: 'true'
+        name: '实时识别',
+        value: '实时识别',
+        checked: 'true',
+        disabled:false,
       },
       {
-        name: '使用2',
-        value: '中国'
+        name: '车牌定位',
+        value: '车牌定位',
+        disabled:false,
       }
     ],
     selectedindex: 0,
@@ -35,6 +39,7 @@ Page({
   },
   start() {
     var that = this
+    var disabled = "radioItems["+ 1 +"].disabled"
     if (that.data.startopen) {
       that.setData({
         show: true,
@@ -42,7 +47,8 @@ Page({
         stopopen: true,
         isshow: false,
         resultinfo: [],
-        flag: false
+        flag: false,
+        [disabled]:true,
       })
       //发送请求
       wx.request({
@@ -66,8 +72,8 @@ Page({
   },
   stop() {
     var that = this
+    var disabled = "radioItems["+ 1 +"].disabled"
     if (that.data.stopopen) {
-
       //发送请求
       wx.request({
         url: "http://127.0.0.1:5000/live_stop?telephone=" + that.data.telephone,
@@ -79,6 +85,7 @@ Page({
             startopen: true,
             resultinfo: res.data.licenselist,
             isshow: true,
+            [disabled]:false,
           })
           if (res.data.len == 0) {
             that.setData({
@@ -110,10 +117,79 @@ Page({
       })
     }
   },
+
+  live_start() {
+    var that = this
+    var disabled = "radioItems["+ 0 +"].disabled"
+    if (that.data.startopen1) {
+      that.setData({
+        show: true,
+        startopen1: false,
+        stopopen1: true,
+        [disabled]:true,
+      })
+      //发送请求
+      wx.request({
+        url: "http://127.0.0.1:5000/live_start?telephone=" + that.data.telephone,
+        method: 'get',
+        success: (res) => {
+          console.log("请求成功")
+        },
+        fail: (res) => {
+          console.log("请求失败")
+        },
+        complete: (res) => {},
+      })
+    } else {
+      wx.showToast({
+        title: '正在识别,请勿重复点击',
+        icon: 'none',
+        duration: 1000,
+      })
+    }
+  },
+
+  live_stop() {
+    var that = this
+    var disabled = "radioItems["+ 0 +"].disabled"
+    if (that.data.stopopen1) {
+
+      //发送请求
+      wx.request({
+        url: "http://127.0.0.1:5000/live_stop?telephone=" + that.data.telephone,
+        method: 'get',
+        success: (res) => {
+          that.setData({
+            show: false,
+            stopopen1: false,
+            startopen1: true,
+            [disabled]:false,
+          })
+          wx.showToast({
+            title: '监测结束',
+            icon: 'none',
+            duration: 1500,
+          })
+        },
+        fail: (res) => {
+          console.log("请求失败")
+        },
+        complete: (res) => {},
+      })
+    } else {
+      wx.showToast({
+        title: '还未开始监测',
+        icon: 'none',
+        duration: 1000,
+      })
+    }
+  },
+
+
   //点击选项
   radioChange(e) {
     var that = this
-    console.log(e)
+    console.log(e.detail)
     const checked = e.detail.value
     const changed = {}
     for (let i = 0; i < that.data.radioItems.length; i++) {
