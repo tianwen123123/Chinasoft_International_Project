33779225460e925ffbe81d4649965ac5b5768dc6
tianwen123123 <2391908465@qq.com>
2022-08-26 12:37:52 +0800

    rby

--- recognization_project/recognization_parent/recognization_backend/src/main/webapp/recognization/pages/liveRecognization/liveRecognization.js
+++ recognization_project/recognization_parent/recognization_backend/src/main/webapp/recognization/pages/liveRecognization/liveRecognization.js
@@ -130,7 +130,7 @@ Page({
       })
       //发送请求
       wx.request({
-        url: "http://127.0.0.1:5000/live_start?telephone=" + that.data.telephone,
+        url: "http://127.0.0.1:5000/yolov5_camera_start?telephone=" + that.data.telephone,
         method: 'get',
         success: (res) => {
           console.log("请求成功")
@@ -156,7 +156,7 @@ Page({
 
       //发送请求
       wx.request({
-        url: "http://127.0.0.1:5000/live_stop?telephone=" + that.data.telephone,
+        url: "http://127.0.0.1:5000/yolov5_camera_stop?telephone=" + that.data.telephone,
         method: 'get',
         success: (res) => {
           that.setData({
@@ -203,6 +203,10 @@ Page({
       }
     }
     that.setData(changed)
+    that.setData({
+      isshow:false,
+      flag:false
+    })
     if (that.data.selectedindex == 0) {
       that.setData({
         method0: true,
